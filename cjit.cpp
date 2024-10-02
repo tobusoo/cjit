890c55ebfd6f1396e61141e9456ea8a5df083887
Dmitry Makogon <d.makogon@g.nsu.ru>
2024-10-02 23:40:32 +0700

    Fix emit llvm

--- cjit.cpp
+++ cjit.cpp
@@ -1,107 +1,92 @@
+#include "clang/Basic/TargetInfo.h"
+#include "clang/Basic/TargetOptions.h"
+#include "llvm/Analysis/CGSCCPassManager.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/PassManager.h"
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Passes/StandardInstrumentations.h"
+#include <llvm/ADT/IntrusiveRefCntPtr.h>
+#include <llvm/IR/Module.h>
+#include <llvm/Support/TargetSelect.h>
+#include <llvm/TargetParser/Host.h>
+
 #include <clang/Basic/DiagnosticOptions.h>
 #include <clang/CodeGen/CodeGenAction.h>
+#include <clang/Driver/Driver.h>
 #include <clang/Frontend/CompilerInstance.h>
 #include <clang/Frontend/CompilerInvocation.h>
 #include <clang/Frontend/TextDiagnosticPrinter.h>
 #include <clang/Lex/PreprocessorOptions.h>
+
 #include <iostream>
-#include <llvm/ADT/IntrusiveRefCntPtr.h>
 
-#include <llvm/IR/Module.h>
-// #include <llvm/Support/Host.h>
-#include <llvm/Support/TargetSelect.h>
-
-using clang::CompilerInstance;
-using clang::CompilerInvocation;
-using clang::DiagnosticConsumer;
-using clang::DiagnosticOptions;
-using clang::DiagnosticsEngine;
-using clang::EmitLLVMOnlyAction;
-using clang::TextDiagnosticPrinter;
-
-using clang::DiagnosticIDs;
-using llvm::ArrayRef;
-using llvm::IntrusiveRefCntPtr;
-using llvm::MemoryBuffer;
-
-using namespace std;
 using namespace llvm;
+using namespace clang;
 
-int main() {
+int main(int argc, char **argv) {
   // Path to the C file
-  string inputPath = "getinmemory.c";
+  std::string InputPath = "simple.c";
 
   // Arguments to pass to the clang frontend
-  vector<const char *> args;
-  args.push_back(inputPath.c_str());
+  std::vector<const char *> Args;
+  Args.push_back(InputPath.c_str());
 
   // Prepare DiagnosticEngine
-  DiagnosticOptions DiagOpts;
-  TextDiagnosticPrinter *textDiagPrinter =
-      new clang::TextDiagnosticPrinter(errs(), &DiagOpts);
-  IntrusiveRefCntPtr<clang::DiagnosticIDs> pDiagIDs;
-  DiagnosticsEngine *pDiagnosticsEngine =
-      new DiagnosticsEngine(pDiagIDs, &DiagOpts, textDiagPrinter);
-
-  // auto DiagOpts = DiagnosticOptions();
-  // DiagOpts.ShowLine = true;
-  // TextDiagnosticPrinter *DiagClient =
-  //     new TextDiagnosticPrinter(llvm::errs(), &DiagOpts);
-  // IntrusiveRefCntPtr<DiagnosticIDs> DiagID(new DiagnosticIDs());
-  // DiagnosticsEngine Diags(DiagID, &DiagOpts);
-
-  // IntrusiveRefCntPtr<DiagnosticOptions> DiagOpts =
-  //     clang::CreateAndPopulateDiagOpts({});
-
-  // TextDiagnosticPrinter *DiagClient
-  //   = new TextDiagnosticPrinter(llvm::errs(), &*DiagOpts);
-  // FixupDiagPrefixExeName(DiagClient, ProgName);
-
-  // IntrusiveRefCntPtr<DiagnosticIDs> DiagID(new DiagnosticIDs());
-
-  // DiagnosticsEngine Diags(DiagID, &*DiagOpts, DiagClient);
-  // auto Diags = CompilerInstance::createDiagnostics(new DiagnosticOptions);
+  IntrusiveRefCntPtr<DiagnosticOptions> DiagOpts(new DiagnosticOptions());
+  auto *TextDiagPrinter = new TextDiagnosticPrinter(errs(), &*DiagOpts);
+  IntrusiveRefCntPtr<DiagnosticIDs> DiagIDs;
+  auto *Diags = new DiagnosticsEngine(DiagIDs, &*DiagOpts, TextDiagPrinter);
 
   // Create the compiler invocation
   auto CI = std::make_shared<CompilerInvocation>();
-  bool Created = clang::CompilerInvocation::CreateFromArgs(*CI, args, *pDiagnosticsEngine);
+  bool Created = CompilerInvocation::CreateFromArgs(*CI, Args, *Diags);
   if (!Created) {
     std::cerr << "Failed to create compiler\n";
     return 1;
   }
 
+  InitializeNativeTarget();
+  InitializeNativeTargetAsmPrinter();
+  InitializeNativeTargetAsmParser();
+
+  auto TripleStr = sys::getProcessTriple();
+  Triple T(TripleStr);
+  if (T.isOSBinFormatCOFF())
+    T.setObjectFormat(Triple::ELF);
+
+  std::cerr << "Target triple is " << T.str() << "\n";
+
+  using clang::TargetOptions;
+
+  auto TheTargetOptions = std::make_shared<TargetOptions>();
+  TheTargetOptions->Triple = T.str();
+
+  auto *TargetInfo = TargetInfo::CreateTargetInfo(*Diags, TheTargetOptions);
+
   // Create the compiler instance
-  clang::CompilerInstance Clang;
+  auto Clang = CompilerInstance();
   Clang.setInvocation(CI);
   Clang.createDiagnostics();
-
-  InitializeAllTargets();
-  // Initialize
-  const std::shared_ptr<clang::TargetOptions> targetOptions =
-      std::make_shared<clang::TargetOptions>();
-  // targetOptions->Triple = string("x86-64");
-  clang::TargetInfo *pTargetInfo =
-      clang::TargetInfo::CreateTargetInfo(*pDiagnosticsEngine, targetOptions);
-  Clang.setTarget(pTargetInfo);
+  Clang.setTarget(TargetInfo);
 
   // Get ready to report problems
-  // Clang.createDiagnostics(DiagOpts.get(), DiagClient);
-  // if (!Clang.hasDiagnostics())
-  //   return 1;
-
-  // Create an action and make the compiler instance carry it out
-  std::unique_ptr<clang::CodeGenAction> Act(new clang::EmitLLVMOnlyAction());
-  if (!Clang.ExecuteAction(*Act))
+  Clang.createDiagnostics();
+  if (!Clang.hasDiagnostics())
     return 1;
 
+  auto Context = std::make_unique<LLVMContext>();
+
+  // Create an action and make the compiler instance carry it out
+  auto Act = std::make_unique<EmitLLVMOnlyAction>(&*Context);
+
+  if (!Clang.ExecuteAction(*Act)) {
+    std::cerr << "Failed to emit module LLVM\n";
+    Diags->dump();
+    return 1;
+  }
+
   // Grab the module built by the EmitLLVMOnlyAction
-  auto module = Act->takeModule();
-
-  // Print all functions in the module
-  for (llvm::Module::FunctionListType::iterator i =
-           module->getFunctionList().begin();
-       i != module->getFunctionList().end(); ++i)
-    printf("%s\n", i->getName().str().c_str());
-
+  auto Module = Act->takeModule();
+  Module->print(dbgs(), /*AAW=*/nullptr);
   return 0;
 }
---
+++ simple.c
@@ -0,0 +1,9 @@
+struct S {
+  int a;
+  int b;
+};
+
+void init(struct S *s) {
+  s->a = 42;
+  s->b = 1337;
+}
